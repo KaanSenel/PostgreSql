3be177c14aa0b99e0b5d7a0281f5f5d7a89b558a
Kaan Şenel <tufankaansenel@gmail.com>
2022-05-24 15:43:41 +0300

    Ödev2-1 eklendi.

---
+++ sqlExamples/Odev2/Odev2-1.sql
@@ -0,0 +1,7 @@
+-- film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit
+-- ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+
+select * from film
+
+where replacement_cost between 12.99 and 16.98
+--between komutunda yazılan değerleri de aldığı için 16.98 yazdım
